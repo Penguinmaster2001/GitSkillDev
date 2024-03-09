62650542ca507e72db6d9a92374ea7473fdb8302
Anthony Cieri <penguinmillion@gmail.com>
2024-03-08 18:21:15 -0800

    fix: negative numbers and 0 no longer cause a RecusionError

--- src/GitPractice/tonysDir/tonysPythonScript.py
+++ src/GitPractice/tonysDir/tonysPythonScript.py
@@ -3,16 +3,20 @@ n0 = int(input("n0: "))
 n1 = int(input("n1: "))
 n  = int(input("n: "))
 
+smallest = min(abs(n0), abs(n1))
+
 memoiry = { }
 
-def fib(n):
-    if n in memoiry:
-        return memoiry[n]
-    if n <= 1:
+def fib(n, depth):
+    if depth == 0:
+        return n;
+    if depth in memoiry:
+        return memoiry[depth]
+    if abs(n) <= smallest:
         return n
     else:
-        memoiry[n] = fib(n - 1) + fib(n - 2)
-        return memoiry[n]
+        memoiry[depth] = fib(n - 1, depth - 1) + fib(n - 2, depth - 1)
+        return memoiry[depth]
     
 
-print(fib(n))
+print(fib(n1, n))
